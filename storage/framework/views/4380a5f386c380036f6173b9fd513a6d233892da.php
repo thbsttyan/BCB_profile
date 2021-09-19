d1cb409f35bc659c73d5b0009c2044af69cb973d
thbsttyan <71692251+thbsttyan@users.noreply.github.com>
2021-09-19 17:56:59 +0300

    Add files via upload

---
+++ storage/framework/views/4380a5f386c380036f6173b9fd513a6d233892da.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-0 mb-5 bg-white rounded">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            <img src  ="./img/svg/logo.svg" alt="BCB" class="header_logo"/>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            
+            
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">О проекте</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Сотрудничество</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Товары и услуги</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Что Вы получите</a>
+              </li>
+              
+            </ul>
+
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Поиск" aria-label="Search">
+              <img class="vector_logo" src="./img/svg/Vector.svg">
+
+              <div class="dropdown">
+                <a href="/" class="union_logo-link" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                  <img src  ="./img/svg/Union.svg" alt="Union" class="union_logo"/>
+                </a>
+                <ul class="dropdown-menu  " aria-labelledby="dropdownMenuButton1" aria-expanded="false" >
+                  <li><a class="dropdown-item" href="#"> <img src  ="./img/svg/1.svg" alt="1" class="logo_1"/> Личный кабинет</a></li>
+                  <li><a class="dropdown-item" href="#"> <img src  ="./img/svg/2.svg" alt="2" class="logo_2"/> Мои покупки</a></li>
+                  <li><a class="dropdown-item" href="#"> <img src  ="./img/svg/3.svg" alt="3" class="logo_3"/> Договор с bcb</a></li>
+                  <li><a class="dropdown-item" href="#"> <img src  ="./img/svg/4.svg" alt="4" class="logo_4"/> Акты</a></li>
+                  <li><a class="dropdown-item" href="#"> <img src  ="./img/svg/5.svg" alt="5" class="logo_5"/> Выплаты</a></li>
+                  <li><a class="dropdown-item" href="#"> <img src  ="./img/svg/6.svg" alt="6" class="logo_6"/> Новости</a></li>
+                  <li><a class="dropdown-item" href="#"> <img src  ="./img/svg/7.svg" alt="7" class="logo_7"/> Отзывы</a></li>
+                  <li><a class="dropdown-item" href="#"> <img src  ="./img/svg/8.svg" alt="8" class="logo_8"/> Чат</a></li>
+                </ul> 
+
+            </form>
+
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+      <main class="main">
+        
+        <div class="nav_hierarchy">
+        <div class="row">
+          <div class="col col-lg-6 ">
+            <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em'%3E%3Cpath d='M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Главная</a></li>
+                <li class="breadcrumb-item"><a href="#">Товары и услуги</a></li>
+                <li class="breadcrumb-item"><a href="#">Компании</a></li>
+                <li class="breadcrumb-item active" aria-current="page">NADUUV.BY</li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+        </div>
+
+        <div class="wrapper">
+          <div class="first_block">
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col col-lg-4 col-md-4 col-4 order-2 order-md-1">
+                <a>
+                  <img src  ="./img/svg/nadduv.svg" alt="Company" class="company_logo"/>
+                </a>
+              </div>
+              <div class="col col-lg-8 col-md-8 col-12 order-3 order-md-2" >
+                  <div class="partner">
+                    
+                    <div class="row">
+                      <div class="col col-xl-2 col-lg-3 col-md-2 col-3 ">
+                        <div class="align-right">
+                          <p class="partner"><b>Партнер</b></p>
+                          <p class="phone"><b>Телефон</b></p>
+                          <p class="mail"><b>E-mail</b></p>
+                        </div>
+                      </div>
+                      <div class="col col-lg-8 col-md-8 col-8 offset-1">
+                          <p class="partner_info">Константин Константинович</p>
+                          <p class="phone_number">+375(29)111 11 11</p>
+                          <p class="mail_address">mail@gmail.com</p>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class="col col-lg-4 col-md-4 col-8 order-2 order-md-3">
+                <div class="company_labels">
+                  <p class="nadduv_label">NADDUV.BY</p>
+                  <div class="id_label">ID 13022000</div>
+                  <a href="#!" class="company_page_link">Перейти на страницу компании</a>
+                </div>
+              </div>
+              <div class="col col-lg-8 col-md-8 col-4 order-4 order-md-4">
+                <button class="became_partner" action="">СТАТЬ ПАРТНЕРОМ</button>
+              </div>
+            </div>
+
+          </div>
+          </div>
+        </div>
+        
+
+        <div class="second_block">
+        <div class="row">
+          <div class="col col-lg-12 col-md-12 col-12 order-md-1 order-2">
+            <div class="wrapper">
+              <table class="table">
+                <thead> 
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Мой рейтинг на сайте</td>
+                    <td class="second_col" width="20%" >  <img src=".\img/svg/Star.svg" class="star" /><img src="./img/svg/Star.svg" class="star" /><img src="./img/svg/Star.svg" class="star" /><img src="./img/svg/Star.svg" class="star" ><img src="./img/svg/NoStar.svg" class="star" /> 4/5</td>
+                  </tr>
+                  <tr>
+                    <td>Лицензия</td>
+                    <td class="second_col" width="20%">Fly</td>
+                  </tr>
+                  <tr>
+                    <td>Покупки в компании</td>
+                    <td class="second_col" width="20%">10</td>
+                  </tr>
+                  <tr>
+                    <td>Мои покупки в компании</td>
+                    <td class="second_col" width="20%">5</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div> 
+        </div>
+
+        <div class="col col-lg-12 col-md-12 col-12 order-md-2 order-1">
+          <div class="wrapper_shadow_block">
+            <div class="shadow_block">
+              <table class="table">
+
+                <tbody>
+                  <tr>
+                    <td class="my_discount_label">Моя скидка:</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td class="table_text">Готово к выводу</td>
+                    <td class="second_col" width="20%">15,30 рублей</td>
+                  </tr>
+                  <tr>
+                    <td>Ожидается начисление</td>
+                    <td class="second_col" width="20%">20,55 рублей</td>
+                  </tr>
+                  <tr>
+                    <td>Заработано за все время</td>
+                    <td class="second_col" width="20%">47,62 рублей</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div> 
+          </div>
+        </div>
+        </div>
+
+      </div>
+      
+      </main>
+
+      
+      
+      <footer class="footer">
+      <div class="container">
+        
+          <div class="float-start offset-sm-1 ">
+            <a href="/" class="down_logo-link">
+                <img src  ="./img/svg/logo.svg" alt="BCB" class="down_logo-link"/>
+            </a>
+            </div>      
+        
+        
+          <div class="float-end offset-lg-3 ">
+              <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">    
+                      <li class="nav-item">
+                          <p class="social_media_label">Мы в социальных сетях:</p>
+                      </li>                        
+                      <li class="nav-item">
+                          <a href="#!" class="inst-link">
+                              <img src  ="./img/svg/inst.svg" alt="inst" class="inst-link"/>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a href="#!" class="fb-link">
+                              <img src  ="./img/svg/fb.svg" alt="fb" class="fb-link"/>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a href="#!" class="vk-link">
+                              <img src  ="./img/svg/vk.svg" alt="vk" class="vk-link"/>
+                          </a>
+                      </li>
+                  </ul>
+                </div>
+              </nav>
+          </div>
+      </div>
+      </footer>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+      <script src="./js/bootstrap.min.js"></script>
+</body>
+</html><?php /**PATH /home/denis/Загрузки/bcb/resources/views/bcb_new/index.blade.php ENDPATH**/ ?>
